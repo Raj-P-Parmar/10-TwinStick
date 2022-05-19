e8e85869b0634d018a02eebe91650a35860b75cd
Raj-P-Parmar <17comp.raaj.parmar@gmail.com>
2022-05-19 10:22:27 +0530

    RollerBall

---
+++ 10-TwinStick/Assets/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	private void Update()
+    {
+        print("H :- " + CrossPlatformInputManager.GetAxis("Horizontal")); 
+        print("V :- " + CrossPlatformInputManager.GetAxis("Vertical")); 
+    }
+}
